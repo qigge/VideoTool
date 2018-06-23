f6c248549ebad0531fc6e90794653af4122aa38a
Eric <wangzeqi2013@foxmail.com>
2018-06-23 10:16:34 +0800

    Initial Commit

---
+++ VideoTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoTool
+//
+//  Created by wang on 2018/6/23.
+//  Copyright © 2018年 acadsoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
